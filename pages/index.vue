fc9e9de03848d924ec59b5af30ad47434fcbc9c0
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2020-07-04 16:59:26 +0700

    :sparkles: Add scss essential mixins and examples

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,9 @@
       <span class="sx-10"></span>
       {{ $t('random-text-using-spacing') }}
     </p>
+    <p class="random-text-using-mixin">
+      {{ $t('random-text-using-mixin') }}
+    </p>
   </b-container>
 </template>
 <script>
@@ -34,5 +37,8 @@ export default {
 <style lang="scss">
 .random-class {
   color: $purple;
+}
+.random-text-using-mixin {
+  @include centerAbsolute('both');
 }
 </style>
